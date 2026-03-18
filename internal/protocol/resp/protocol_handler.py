8affaec3b833dda9bf52ffe661bf72cb4005ab28
iamnuked <qjawls6262@gmail.com>
2026-03-18 14:10:18 +0900

    feat: add protocol entrypoint and test env

---
+++ internal/protocol/resp/protocol_handler.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from internal.command.command import Command
+from internal.command.errors import CommandError
+from internal.command.parser import CommandParser
+from internal.command.validator import CommandValidator
+from internal.protocol.resp.hello_handler import HelloHandler
+from internal.protocol.resp.request_decoder import RespRequestDecoder
+from internal.protocol.resp.types import RespSimpleError, RespValue
+
+
+@dataclass(frozen=True)
+class ProtocolHandlerResult:
+    command: Command | None = None
+    response: RespValue | None = None
+
+    def has_immediate_response(self) -> bool:
+        return self.response is not None
+
+
+class ProtocolHandler:
+    def __init__(
+        self,
+        decoder: RespRequestDecoder | None = None,
+        parser: CommandParser | None = None,
+        validator: CommandValidator | None = None,
+        hello_handler: HelloHandler | None = None,
+    ) -> None:
+        self._decoder = decoder or RespRequestDecoder()
+        self._parser = parser or CommandParser()
+        self._validator = validator or CommandValidator()
+        self._hello_handler = hello_handler or HelloHandler()
+
+    def handle(self, data: bytes) -> ProtocolHandlerResult:
+        request = self._decoder.decode(data)
+        command = self._parser.parse(request)
+        self._validator.validate(command)
+
+        if command.name == "HELLO":
+            version = int(command.arguments[0])
+            return ProtocolHandlerResult(response=self._hello_handler.handle(version))
+
+        return ProtocolHandlerResult(command=command)
+
+    def handle_with_error_response(self, data: bytes) -> ProtocolHandlerResult:
+        try:
+            return self.handle(data)
+        except CommandError as error:
+            return ProtocolHandlerResult(response=RespSimpleError(message=error.message))
---
+++ tests/test_protocol_handler.py
@@ -0,0 +1,41 @@
+from internal.command.command import Command
+from internal.protocol.resp.protocol_handler import ProtocolHandler
+from internal.protocol.resp.types import RespMap, RespSimpleError
+
+
+def test_handle_returns_immediate_response_for_hello() -> None:
+    handler = ProtocolHandler()
+
+    result = handler.handle(b"*2\r\n$5\r\nHELLO\r\n:3\r\n")
+
+    assert result.command is None
+    assert isinstance(result.response, RespMap)
+    assert result.has_immediate_response() is True
+
+
+def test_handle_returns_command_for_non_hello_request() -> None:
+    handler = ProtocolHandler()
+
+    result = handler.handle(b"*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n")
+
+    assert result.response is None
+    assert result.command == Command(name="GET", arguments=("key",))
+    assert result.has_immediate_response() is False
+
+
+def test_handle_with_error_response_converts_protocol_failures() -> None:
+    handler = ProtocolHandler()
+
+    result = handler.handle_with_error_response(b"+PING\r\n")
+
+    assert result.command is None
+    assert result.response == RespSimpleError(message="protocol error")
+
+
+def test_handle_with_error_response_converts_validation_failures() -> None:
+    handler = ProtocolHandler()
+
+    result = handler.handle_with_error_response(b"*2\r\n$6\r\nEXPIRE\r\n$3\r\nkey\r\n")
+
+    assert result.command is None
+    assert result.response == RespSimpleError(message="wrong number of arguments")
